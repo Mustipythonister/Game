865d5cc3c9b223ce1294e48381d3660b2660caa1
Mustaph <84411486+Mustipythonister@users.noreply.github.com>
2022-05-07 18:52:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function show(p1,p2,p3){
+	var page1 = document.getElementById(p1);
+	var page2 = document.getElementById(p2);
+	var page3 = document.getElementById(p3);
+	page1.style.display="block";
+	page2.style.display="none";
+	page3.style.display="none";
+}
+function love(){
+	alert("Awch stop clicking me.");
+}
